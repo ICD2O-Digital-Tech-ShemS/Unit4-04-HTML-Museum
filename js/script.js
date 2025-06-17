bbc44727e33ab213c87084ad9c2cf35aba12cac2
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-06-17 14:55:25 +0000

    Update 5 Assign

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+"use strict";
+
+function Guess() {
+let age = parseInt(document.getElementById("age").value);
+let day = document.getElementById("day").value;
+let ans = "";
+
+if (age <= 5 || age >= 95) {
+    ans = "Entry is free";
+} else if (age >= 12 && age <= 21) {
+    ans = "You get a discount";
+} else if (day === "Tuesday" || day === "Thursday") {
+    ans = "You get a discount";
+} else {
+    ans = "Pay regular price";
+}
+
+document.getElementById("ans").innerHTML = ans;
+}
